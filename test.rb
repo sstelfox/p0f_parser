f7ef1e2a9290be7294ee1e3d9ca71f32a144f4c4
Jeremy Waters <jwaters@0x378.net>
2013-08-12 13:00:04 -0400

    Moved the host records into fixtures matching their names

--- test.rb
+++ test.rb
@@ -3,6 +3,9 @@ require 'json-schema'
 require 'yaml'
 
 schema = JSON.parse(File.read('./lib/p0f_parser/schemas/host.json'))
-data = YAML.load(File.read('./test.yml'))
+data1 = YAML.load(File.read('./spec/fixtures/full_host.yml'))
+data2 = YAML.load(File.read('./spec/fixtures/min_host.yml'))
 
-puts JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
+# Weird... either one works but they don't work together... somebody is storing state they shouldn't be...
+puts JSON::Validator.fully_validate(schema, data1)
+#puts JSON::Validator.fully_validate(schema, data2)
