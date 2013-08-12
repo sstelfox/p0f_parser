70540ffb642b4a585687b560ee7075d146cd5850
Jeremy Waters <jwaters@0x378.net>
2013-08-12 11:06:16 -0400

    Closer to validation...

--- test.rb
+++ test.rb
@@ -5,4 +5,4 @@ require 'yaml'
 schema = JSON.parse(File.read('./lib/p0f_parser/schemas/host.json'))
 data = YAML.load(File.read('./test.yml'))
 
-puts JSON::Validator.validate(schema, data)
+puts JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
